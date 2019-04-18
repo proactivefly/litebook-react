3e2fac2778995d8863cae28a1be21f0d7cb35d61
ecstack <ecstack@sina.com>
2019-04-18 09:25:17 +0800

    Git commend

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { BrowserRouter, Route } from 'react-router-dom';
+import React, {Component} from 'react';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 import Header from './common/header';
 import Home from './pages/home';
 import Detail from './pages/detail/loadable.js';
@@ -11,16 +11,16 @@ import store from './store';
 class App extends Component {
   render() {
     return (
-    	<Provider store={store}>
-      	<BrowserRouter>
-      		<div>
-            <Header />
-      			<Route path='/' exact component={Home}></Route>
+      <Provider store={store}>
+        <BrowserRouter>
+          <div>
+            <Header/>
+            <Route path='/' exact component={Home}></Route>
             <Route path='/login' exact component={Login}></Route>
             <Route path='/write' exact component={Write}></Route>
-      			<Route path='/detail/:id' exact component={Detail}></Route>
-      		</div>
-      	</BrowserRouter>
+            <Route path='/detail/:id' exact component={Detail}></Route>
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,5 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import './style.js';
 import './statics/iconfont/iconfont';
-
+// 将APP组件挂在到id=root的元素中
 ReactDOM.render(<App />, document.getElementById('root'));
